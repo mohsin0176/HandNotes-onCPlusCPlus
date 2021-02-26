70ba48d3bf2e13266eda796de76dcc8e897b87e3
mohsin0176 <mohsin2010ju@gmail.com>
2021-02-26 04:00:49 -0800

    Escape Sequenece and BackSlash Character

---
+++ datatypesvariables/12.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+using namespace std;
+ 
+int main()
+{
+    cout<<"A\nB"<<endl;
+    cout<<"A\tB"<<endl;
+	cout<<"\"JU\""<<endl;
+	cout<<"\'JU\""<<endl;
+
+
+	return 0;
+}
