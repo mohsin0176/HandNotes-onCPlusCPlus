9c182a9e481f05f7ca08b26c1cd7553e12cae6e6
mohsin0176 <mohsin2010ju@gmail.com>
2021-02-26 03:23:21 -0800

    Dynamic Initiliazation

---
+++ datatypesvariables/2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double radius=10;
+    double height=10;
+    double volume=3.1416*radius*radius*height;
+    cout<<volume;
+	return 0;
+}
