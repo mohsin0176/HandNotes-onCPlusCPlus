3c371bf99dce9a4f01c3fb5ce816b8df3902f651
mohsin0176 <mohsin2010ju@gmail.com>
2021-02-26 04:03:13 -0800

    unsigned modifier

---
+++ datatypesvariables/13.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+using namespace std;
+ 
+int main()
+{
+    
+    int var1=2000;
+    unsigned int var2=200;
+    var1=(var1*2)/3;
+    var2=(var2*2)/3;
+    cout<<"signed variable"<<var1<<endl;
+    cout<<"unsigned variable"<<var2<<endl; 
+	return 0;
+}
