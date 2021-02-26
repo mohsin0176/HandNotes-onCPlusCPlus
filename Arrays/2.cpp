a06385fb44bf19bae675f83062812fe31dd6b056
mohsin0176 <mohsin2010ju@gmail.com>
2021-02-26 04:24:40 -0800

    Initiliazation of an array

---
+++ Arrays/2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+using namespace std;
+ 
+int main()
+{
+    
+     int num[10]={10,20,30,40,50,60,70,80,90,100};
+     for(int i=0;i<10;i++)
+     {
+     	cout<<num[i]<<endl;
+     }
+
+     
+	return 0;
+}
