0b60de2cc0512c3f255544d500e82787a55c09ee
mohsin0176 <mohsin2010ju@gmail.com>
2021-02-26 03:57:41 -0800

    Ascii value of given character

---
+++ datatypesvariables/11.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+using namespace std;
+ 
+int main()
+{
+ 
+     char ch;
+     cout<<"Enter Any Character"<<endl;
+     cin>>ch;
+     cout<<"ASCII Value of"<<ch<<"is"<<int(ch)<<endl;
+	return 0;
+}
