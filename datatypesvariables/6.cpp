77268461218c3edd5ee0a27b827e07bcf75258eb
mohsin0176 <mohsin2010ju@gmail.com>
2021-02-26 03:35:46 -0800

    user defined type declare typedef

---
+++ datatypesvariables/6.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+     typedef int integer;
+typedef float real;
+    integer a=10;
+    real b=10.23;
+    cout<<a<<endl;
+    cout<<b<<endl; 
+
+	return 0;
+}
