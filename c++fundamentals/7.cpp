a4aa9beb205fb4d921d4ebd874aa618910dab13c
mohsin0176 <mohsin2010ju@gmail.com>
2021-02-26 02:38:21 -0800

    Use endl-setw

--- c++fundamentals/7.cpp
+++ c++fundamentals/7.cpp
@@ -6,10 +6,10 @@ int main()
 {
 	int a=12,b=123,c=1234;
 	cout<<a<<endl;
-	cout<<b<<endl;
+	cout<<b<<endl;//endl
 	cout<<c<<endl;
 	cout<<setw(7)<<a<<endl;
 	cout<<setw(7)<<b<<endl;
-	cout<<setw(7)<<c<<endl;
+	cout<<setw(7)<<c<<endl; //setw()
 	return 0;
 }
