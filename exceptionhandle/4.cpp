03e8147577d93b07f20e8cd5788ddb48b76cf6ef
mohsin0176 <mohsin2010ju@gmail.com>
2021-02-27 08:36:24 -0800

    multiple catch block

---
+++ exceptionhandle/4.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#define PI 3.1416
+using namespace std;
+
+void test(int x)
+{
+	try
+	{
+		if (x==0)
+		{
+			throw 'B';
+		}
+		else if(x==1)
+		{
+			throw x;
+		}
+		else if(x==2)
+		{
+			throw 1;
+		}
+		cout<<"End of try Block"<<endl;
+	}
+    
+    catch(char c)
+    {
+    	cerr<<"Caught a character"<<endl;
+    }
+	catch(int i)
+	{
+		cerr<<"caught a int"<<c<<endl;
+	}
+	catch(double d)
+	{
+		cerr<<"Caught a double"<<d<<endl;
+	}
+    cout<<"End of try Catch system"<<endl;
+} 
+
+int main()
+{
+
+	cout<<"Testing multiple Catches"<<endl;
+	cout<<"x==0"<<endl;
+	test(0);
+	cout<<"x==1"<<endl;
+	test(1);
+	cout<<"x==2"<<endl;
+	test(2);
+	cout<<"x==3"<<endl;
+	test(3);
+
+
+	return 0;
+}
