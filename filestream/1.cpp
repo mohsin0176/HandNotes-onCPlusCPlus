caa02fbcbc361c999990387a2b5cf64478092fb4
mohsin0176 <mohsin2010ju@gmail.com>
2021-02-27 11:17:07 -0800

    File Create and write

---
+++ filestream/1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{
+    ofstream("test.txt");
+	out<<"i am mohsin\n"<<endl;
+	out<<"jahangirnagar university\n"<<endl;
+	out<<"Information technology\n"<<endl;
+	out<<"Savar,Dhaka-1342\n"<<endl;
+	cout<<"Written to file"<<endl;
+	return 0;
+}
