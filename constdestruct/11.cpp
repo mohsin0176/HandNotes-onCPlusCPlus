469b5db9bef5bde2c333db0bc139a6bd6cc70158
mohsin0176 <mohsin2010ju@gmail.com>
2021-02-27 07:38:12 -0800

    use copy constructor

---
+++ constdestruct/11.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#define PI 3.1416
+using namespace std;
+class Distance
+{
+private:
+	int feet;
+	float inches;
+public:
+	Distance()
+	{
+		feet=0;
+		inches=0;
+	}
+	Distance(int ft,float in)
+	{
+		feet=ft;
+		inches=in;
+	}
+	Distance(Distance &d)
+	{
+		feet=d.feet;
+		inches=d.inches;
+	}
+
+	void showDist()
+	{
+		cout<<feet<<inches;
+	}
+};
+
+int main()
+{
+    Distance d1(10,6.5);
+    Distance d2(d1);
+    Distance d3=d1;
+    d1.showDist();
+    d2.showDist();
+    d3.showDist();
+	return 0;
+}
