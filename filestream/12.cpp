ae6b4e5e25febe1d5629f4d75cb9a5758743fd44
mohsin0176 <mohsin2010ju@gmail.com>
2021-02-28 07:51:45 -0800

    Explain Command Line Arguments

---
+++ filestream/12.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#define PI 3.1416
+using namespace std;
+
+ 
+
+int main(int argc,char* argv[])
+{
+    
+    cout<<"No of Arguments"<<argc<<endl;
+    for (int i = 0; i < argc; ++i)
+    {
+    	cout<<argv[i]<<endl;
+    }
+
+	return 0;
+}
