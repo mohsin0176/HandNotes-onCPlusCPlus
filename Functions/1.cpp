9a1be35504d57b2408b9c0c4c542c42114588188
mohsin0176 <mohsin2010ju@gmail.com>
2021-02-28 08:24:35 -0800

    Simple function

---
+++ Functions/1.cpp
@@ -0,0 +1,23 @@
+
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#define PI 3.1416
+using namespace std;
+
+void show(int x);
+void show(int x)
+{
+	cout<<x<<endl;
+}
+int main()
+{
+    int a=10;
+    show(a);
+
+	return 0;
+}
