d9b0934c71580601ec0ec01c8d3c999630302c89
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-02 05:39:18 -0800

    Overload Template Function

---
+++ tempalte/4.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+template<class T>
+void show(T a)
+{
+	cout<<a;
+}
+void show(int a)
+{
+	cout<<a;
+
+}
+int main()
+{  
+	show(10);
+	show('B');
+	show(12.34);
+	show("BIIT");
+
+	return 0;
+}
