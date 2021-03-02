13e4c6c16c00df043b660107cc4f624a61adca19
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-02 05:37:15 -0800

    Function template with multiple parameter

---
+++ tempalte/3.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+template<class T1,class T2>
+void show(T1 a,T2 b)
+{
+	cout<<a<<b;
+}
+int main()
+{
+   show(10,'B');
+   show('B',20.75);
+   show(10.25,'B');
+	return 0;
+}
