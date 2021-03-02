ef4ec6b32914adeae3383db61f531c808e9a6c5f
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-02 05:33:24 -0800

    Function templae

---
+++ tempalte/1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+template <class T>
+void show(T a)
+{
+	cout<<a;
+}
+
+int main()
+{
+    show('B');
+    show(10);
+    show(12.34);
+    show("BIIT");
+	return 0;
+}
