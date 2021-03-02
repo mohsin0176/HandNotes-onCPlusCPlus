78149e7d91e50fe8f36e1fd334628071a0a21c86
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-02 04:54:19 -0800

    const argument to function

---
+++ Functions/19.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+void show(const int);
+void show(const int b)
+{
+	cout<<b;
+}
+int main()
+{
+   int a=10;
+   show(a);
+   cout<<a;
+
+	return 0;
+}
