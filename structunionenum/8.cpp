94a48a17eebc43849335b166d8d909439690fcff
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-02 09:48:27 -0800

    Array of structure

---
+++ structunionenum/8.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+struct Distance
+{
+	int feet;
+	float inches;
+};
+
+
+int main()
+{
+    Distance d1[5];
+    for (int i = 0; i < 5; ++i)
+    {
+    	cin>>d[i].feet>>d[i].inches;
+    }
+    for (int i = 0; i < 5; ++i)
+    {
+    	cout<<d[i].feet<<d[i].inches;
+    }
+	 
+	return 0;
+}
