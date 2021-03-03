26022828540ded3b1ac818c5b62722645c255923
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-03 07:19:27 -0800

    Input multiple word in string by cin.get() function

---
+++ Strings/4.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+
+int main()
+{
+     char name[25];
+     cin.getline(name,25);
+     cout<<name;
+	return 0;
+}
