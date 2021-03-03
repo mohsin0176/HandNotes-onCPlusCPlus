62697cf44a874ee6f8c738552d7e0cd0105849c5
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-03 07:14:11 -0800

    Input and display string

---
+++ Strings/1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{
+    char c[25];
+    gets(c);
+    cout<<c;
+    puts(c);
+	return 0;
+}
