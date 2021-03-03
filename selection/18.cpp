30a607d5ff3101564731d559a24c33ba889155db
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-03 09:49:08 -0800

    Ternary Operator to find max among 3 number

---
+++ selection/18.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{
+    int a,b,c,max;
+    max=((a>b)?((a>c)?a:c):((b>c)?b:c));
+    cout<<max;
+	return 0;
+}
