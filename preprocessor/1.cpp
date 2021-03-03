46838b6e29114fe3c0972c0d8e85336f8c8748c2
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-03 09:53:22 -0800

    simple macro subxtitution

---
+++ preprocessor/1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{
+   cout<<PI<<endl;
+	return 0;
+}
