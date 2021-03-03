64183fb26d4d8a5a17c3c675f68534a99b19d4a9
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-03 09:50:28 -0800

    Ternary Operator to find value of number

---
+++ selection/19.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{
+    int a,b,x=0,y=0;
+    cin>>a>>b;
+    (a>b)?x:y=25;
+    cout<<x<<y;
+	return 0;
+}
