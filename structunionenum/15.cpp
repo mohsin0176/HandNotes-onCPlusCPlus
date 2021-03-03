328aa9a68032f25f2499f34bf4d78571770e97cb
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-03 06:58:36 -0800

    Explain Enum

---
+++ structunionenum/15.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+enum days_of_week{sun,mon,tues,wednesday,thu,fri,sat};
+
+int main()
+{
+    
+    days_of_week day1,day2;
+    day1=mon;
+    day2=thu;
+    int dff=day2-day1;
+    cout<<diff;
+    if (day1<day2)
+    {
+    	cout<<"Day1 comes before Day2"<<endl;
+    }
+	return 0;
+}
