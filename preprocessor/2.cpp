83c951135200668c16a5c5b5d9a048c8d2efa297
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-03 09:54:16 -0800

    simple macro subxtitution

---
+++ preprocessor/2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{
+    float r,a;
+    cin>>r;
+    a=PI*r*r;
+    cout<<a;
+	return 0;
+}
