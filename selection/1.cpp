d0a89c3e5fbe7c331176ccc068dde2b2b1fbaa6e
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-03 08:36:02 -0800

    simple if

---
+++ selection/1.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{
+   
+   int x;
+   cin>>x;
+   if (x>100)
+   {
+   	cout<<"x is big than 100"<<endl;
+   }
+	return 0;
+}
