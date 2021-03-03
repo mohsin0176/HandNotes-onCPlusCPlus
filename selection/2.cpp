332073d78c563933d9ec99cbd9c14869ffad7c44
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-03 08:37:34 -0800

    simple if-else

---
+++ selection/2.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{
+    int n;
+    cin>>n;
+    if (n>100)
+    {
+    	cout<<"n is big than 100"<<endl;
+    }
+    else
+    {
+    	cout<<"n is small than 100"<<endl;
+    }
+	return 0;
+}
