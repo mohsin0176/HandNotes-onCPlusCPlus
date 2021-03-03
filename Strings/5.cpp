2aaa93ea0fc97eab2f6adf284ed1aaa0fca89ff4
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-03 07:21:44 -0800

    length of string by strlen function

---
+++ Strings/5.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+
+int main()
+{
+     char str[51];
+     int length;
+     cin.line(str,50);
+     length=strlen(str);
+     cout<<length;
+     
+	return 0;
+}
