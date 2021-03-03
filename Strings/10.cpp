355669f4f764edd1b8bead5ec646c3eb33fa55cc
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-03 07:45:23 -0800

    string without using strlen function

---
+++ Strings/10.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+const int max=50;
+
+int main()
+{
+     char str[max];
+     int i,len;
+     gets(str);
+ 	for (int i = 0; str[i] !='\0'; ++i)
+ 	{
+ 	len=i;
+ 	}
+     cout<<len;
+	return 0;
+}
