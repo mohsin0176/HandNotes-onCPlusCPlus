f58bbdea456acedb426c8d80363a995ed4c03221
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-03 09:55:58 -0800

    argument with macro subxtitution

---
+++ preprocessor/3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+#define MIN(a,b) (((a)<(b))?a:b)
+
+int main()
+{  
+	int x,y;
+	x=10;
+	y=20;
+	cout<<MIN(x,y)<<endl;
+
+	return 0;
+}
