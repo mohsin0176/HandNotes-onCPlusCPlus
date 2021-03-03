b72426d130a8740bd6f6d947c614cb457b28e7d1
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-03 08:12:39 -0800

    Array of string

---
+++ Strings/17.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{
+   char name[5][20]={"Ritikia","Shilpa","Orpita","Bhavna","Swati"};
+   int i;
+   for (int i = 0; i < 5; ++i)
+   {
+   	 cout<<name[i];
+   }
+	return 0;
+}
