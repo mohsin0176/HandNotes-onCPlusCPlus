32afb6005922c174cb703abf4672f4adc6b6aa11
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-03 07:18:15 -0800

    Input multiple line in string by cin.get() function

---
+++ Strings/3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+const int max=200;
+
+int main()
+{
+     char str[max];
+     cin.get(str,max,'$');
+     cout<<str;
+	return 0;
+}
