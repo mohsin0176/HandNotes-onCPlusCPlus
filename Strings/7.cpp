48a42f9197ffbbf68b75689133837f77f0eae74d
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-03 07:26:29 -0800

    copy string by strcat function

---
+++ Strings/7.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+ 
+
+int main()
+{
+      char name[60];
+      strcpy(name,"BIIT");
+      strcat(name,"");
+      strcat(name,"Computer");
+      strcat(name,"");
+      strcat(name,"Education");
+      cout<<name;
+	return 0;
+}
