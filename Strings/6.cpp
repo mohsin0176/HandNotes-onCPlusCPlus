5e855a9ec7e1554c9c4e02d71ab24c1f8c8f11ae
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-03 07:23:39 -0800

    copy string by strcpy function

---
+++ Strings/6.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+const int max=80;
+
+int main()
+{
+     char str1[]="BIIT Computer Education";
+     char str2[max];
+     strcpy(str2,str1);
+     cout<<str2
+    cout<<str;
+	return 0;
+}
