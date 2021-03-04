2a4e47ec120fe6f6bd5fc011dc08e78b17794c50
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-04 08:19:01 -0800

    use compiler control directives #undef

---
+++ preprocessor/10.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+#define VAL 40
+#ifdef VAL
+#undef VAL
+#endif
+#define VAL 50
+int main()
+{  
+
+	cout<<VAL;
+
+	return 0;
+}
+
