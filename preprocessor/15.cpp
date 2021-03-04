22b7503c023dcfab487a4c5a6b6902e882b6d6bc
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-04 08:30:18 -0800

    String Sizing # operator

---
+++ preprocessor/15.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+#define MKSTR(x) #x
+
+int main()
+{
+    cout<<MKSTR(BIIT Computer Education)<<endl;
+	return 0;
+}
+
