53c129f6ff9aa71f65b736d7fcd1ca9c796bd2ff
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-04 08:33:34 -0800

    use of __LINE__,__FILE__,__DATE__,__TIME__

---
+++ preprocessor/17.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{   
+
+cout<<__LINE__<<endl;
+cout<<__FILE__<<endl;
+cout<<__DATE__<<endl;
+cout<<__TIME__<<endl;
+return 0;
+}
+
