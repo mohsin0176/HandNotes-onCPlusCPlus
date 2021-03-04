205bfe333fffa4eed20e771d6a230354a653daed
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-04 08:39:52 -0800

    Simple Pointer vary at different point of time

---
+++ pointers/2.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{  
+	int a=10,b=20,c=30;
+	int *p;
+	p=&a;
+	cout<<*p<<endl;
+	p=&b;
+	cout<<*p<<endl;
+	p=&c;
+	cout<<*p<<endl;
+
+	return 0;
+}
+
