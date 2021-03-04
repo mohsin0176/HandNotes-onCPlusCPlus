7a4dc74e8cc14c48a4ac19136b9732ddb9642fa5
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-04 08:07:50 -0800

    include proprocessor

---
+++ preprocessor/5.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+
+using namespace std;
+
+#include "myheader.h"
+int main()
+{
+   int a=20,b=10,res;
+   res=add(a,b);
+   cout<<res;
+   res=sub(a,b);
+   cout<<res;
+	return 0;
+}
+
