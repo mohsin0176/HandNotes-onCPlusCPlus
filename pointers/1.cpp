435f15f61c46965bf3e94020472eecc7916916a8
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-04 08:38:06 -0800

    Simple Pointer

---
+++ pointers/1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{
+    int a=10;
+    int *p;
+    p=&a;
+    cout<<a<<endl;
+    cout<<p<<endl;
+    cout<<&a<<endl;
+    cout<<*p<<endl;
+    cout<<&p<<endl;
+	return 0;
+}
+
