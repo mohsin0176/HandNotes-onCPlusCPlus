a096c9132ffae6018b12d6f1d626bb27fff3d8c7
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-04 08:24:17 -0800

    define error preprocessor

---
+++ preprocessor/12.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+//#define USA 1
+//#define IND 1
+
+#if(!defined(USA)||!defined(IND))
+#error ERROR:NO_CURRENCY rate is specified
+#endif
+int main()
+{
+   int amt;
+   amt=1000;
+   cout<<amt;
+	return 0;
+}
+
