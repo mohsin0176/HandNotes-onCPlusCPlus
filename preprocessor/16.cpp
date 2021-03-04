395f6163c2f6dd294386990feebfde717d9e92f1
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-04 08:31:34 -0800

    Token Pasting ## operator

---
+++ preprocessor/16.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+#define concat(a,b) a##b
+int main()
+{
+    int xy=100;
+    cout<<concat(x,y);
+	return 0;
+}
+
