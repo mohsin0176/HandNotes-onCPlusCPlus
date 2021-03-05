084a195e881544a049ddc56b7ddce5b1bd633552
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-05 10:34:01 -0800

    explain void pointer

---
+++ pointers/16.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{
+
+
+    int i;
+    float f;
+    int *ip;
+    float *fp;
+    void *vp;
+
+    ip=&i;
+    fp=&f;
+    vp=&i;
+    vp=&f;
+	return 0;
+}
