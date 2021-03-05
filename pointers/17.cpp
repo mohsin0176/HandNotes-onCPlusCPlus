4887e8d949cc41bc82e32434a93a6cbdfc644e89
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-05 10:36:33 -0800

    explain string-pointer

---
+++ pointers/17.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{
+    
+    char s1[25]="BIIT";
+    char *ps1="BIIT";
+    char s2[25];
+    char *ps2;
+    cout<<s1<<endl;
+    cout<<ps1<<endl;
+    ps2=ps1;
+    cout<<ps2<<endl;
+    ps1++;
+    cout<<ps1<<endl;
+	return 0;
+}
