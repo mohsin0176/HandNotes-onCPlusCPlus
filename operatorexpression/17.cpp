5e5b58cf1f23558a2abdfc89ccff3424979e7b6e
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-06 08:14:06 -0800

    automatic or implicit conversion type casting

---
+++ operatorexpression/17.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{  
+    int a=10,b=20;
+    float c=10.50;
+    long d=20L,e;
+    e=((a+c)*d)+b;
+    cout<<e;
+	return 0;
+}
