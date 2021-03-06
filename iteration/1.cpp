0b0dd0d2c60f201a035b6953da7289eef2068b6a
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-06 08:26:42 -0800

    simple for loop

---
+++ iteration/1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{
+    int i;
+    for (int i = 0; i <= 10; ++i)
+    {
+    	cout<<i;
+
+    }
+	return 0;
+}
