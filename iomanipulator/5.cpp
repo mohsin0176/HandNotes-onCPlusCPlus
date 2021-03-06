39910d10fbfe01f3be60b35b678e93a5e55607e6
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-06 09:40:44 -0800

    Cout.write function

---
+++ iomanipulator/5.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{
+    char nm[25];
+    cin.getline(nm,25);
+    cout.write("Name:",7);
+    cout.write(nm,strlen(nm));
+	return 0;
+}
