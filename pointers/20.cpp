3b5b35c39f57fb586b14f3c14943867cb1438654
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-06 05:24:44 -0800

    Dynamic Memory management using new-delete operator

---
+++ pointers/20.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{
+    int *p;
+    p=new int();
+    *p=10;
+    cout<<*p<<endl;
+    delete p;
+	return 0;
+}
