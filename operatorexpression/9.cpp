8f55ba351d3208a654491c4ef68c9d6368f7392e
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-06 07:57:51 -0800

    sizeof operator

---
+++ operatorexpression/9.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{
+    
+cout<<sizeof(int);
+cout<<sizeof(short);
+cout<<sizeof(float);
+cout<<sizeof(double);
+cout<<sizeof(long);
+cout<<sizeof(char);    
+	return 0;
+}
