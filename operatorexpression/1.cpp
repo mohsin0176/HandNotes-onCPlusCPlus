a6de3a9691d9c0aa242f276a363899edbc88a641
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-06 07:42:05 -0800

    Arithmatic operator

---
+++ operatorexpression/1.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{
+    int a,b,res;
+    cin>>a>>b;
+	res=a+b;
+	cout<<res;
+	res=a-b;
+	cout<<res;
+	res=a*b;
+	cout<<res;
+	res=a/b;
+	cout<<res;
+	res=a%b;
+	cout<<res;
+	return 0;
+}
