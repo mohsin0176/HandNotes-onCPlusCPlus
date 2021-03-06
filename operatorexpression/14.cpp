b9bafa66e737f547784742a13e41d5f2f3db8a22
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-06 08:08:12 -0800

    ternary operator

---
+++ operatorexpression/14.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{
+    int a,b,max;
+    cin>>a>>b;
+    max=(a>b)?a:b;
+    cout<<max;
+	return 0;
+}
