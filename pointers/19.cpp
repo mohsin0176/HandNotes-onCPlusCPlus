2dcda2486344582ebbcfe2ee959c12036334d320
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-06 05:23:25 -0800

    Array of pointer to string

---
+++ pointers/19.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{
+  
+    char *ps[3]={"BIIT","Computer","Education"};
+    for (int i = 0; i < 3; ++i)
+    {
+    	cout<<ps[i]<<endl;
+    }
+	return 0;
+}
