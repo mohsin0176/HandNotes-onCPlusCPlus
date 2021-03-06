27de9cc13c4f7b76b5d99e1913354755f4a4f409
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-06 08:07:18 -0800

    explain expression

---
+++ operatorexpression/13.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{
+    int w,x,y,z,p;
+    cin>>w>>x>>y>>z;
+    p=(w+x)/(y-z);
+    cout<<p;
+	return 0;
+}
