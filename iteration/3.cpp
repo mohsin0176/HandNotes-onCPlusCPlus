072317765fda4249be16f8236fcb6342d5353c65
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-06 08:28:37 -0800

    simple do-while loop

---
+++ iteration/3.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{
+    int n=1;
+    do
+    {
+    	cin>>n;
+    }
+    while(n!=0);
+	return 0;
+}
