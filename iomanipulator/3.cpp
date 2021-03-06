b7c7b127fc1d10749cf985d7d21680a68fe3f0b7
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-06 09:37:22 -0800

    Cout function

---
+++ iomanipulator/3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{
+    char ch;
+    cin.get(ch);
+    cout.put(ch);
+	return 0;
+}
