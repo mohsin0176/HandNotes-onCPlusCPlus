d920ff1024924dbe5fe41eb4b5fb585c026ae01b
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-06 05:41:05 -0800

    Static-Early-Compile Time Binding

---
+++ pointers/26.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+class Shape
+{
+protected:
+	float width,height;
+public:
+	Shape(float a=0,float b=0)
+	{
+		width=a;
+		height=b;
+	}
+	void area()
+	{
+		cout<<0;
+	}
+};
+class Rectangle:public Shape
+{
+Rectangle(float a=0,float b=0):Shape(a,b){};
+void area()
+{
+	cout<<width*height;
+}
+};
+class Triangle:public Shape
+{
+Triangle(float a=0,float b=0):Shape(a,b){};
+void area()
+{
+	cout<<width*height;
+}
+};
+int main()
+{   
+	Shape *shape;
+	Rectangle rec(10,7);
+	Triangle tri(10,5);
+	shape=&rec;
+	shape->area();
+	shape=&tri;
+	shape->area();
+
+
+	return 0;
+}
