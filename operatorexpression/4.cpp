901e834e7808e327f17181be79d2bfade9038055
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-06 07:50:18 -0800

    Assignmnet operator

---
+++ operatorexpression/4.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{
+    int a=b=c=d=e=f=10;
+	cout<<a<<b<<c<<endl;
+	cout<<d<<e<<f<<endl;    
+	return 0;
+}
