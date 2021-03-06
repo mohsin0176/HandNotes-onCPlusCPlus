ff293d8f1d322538c2092dee7301615cbbcb482b
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-06 07:54:15 -0800

    prefix/postfix deccrement operator

---
+++ operatorexpression/7.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{
+    int a=10;
+    int b=20;
+    int c;
+    c=(--a)+(--b);
+    cout<<a<<b<<c;
+	return 0;
+}
