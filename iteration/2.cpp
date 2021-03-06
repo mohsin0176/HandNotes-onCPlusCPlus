f75d09a110388dba3ed4db3958e920ac6128c4f7
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-06 08:27:33 -0800

    simple while loop

---
+++ iteration/2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{  
+
+   int n=1;
+   while(n!=0)
+   {
+   	cin>>n;
+   }
+	return 0;
+}
