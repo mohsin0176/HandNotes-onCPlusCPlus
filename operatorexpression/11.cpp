b7e8642b3cfb866e5c0277da1cba1493e14a8402
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-06 08:02:35 -0800

    use of comma operator

---
+++ operatorexpression/11.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{
+    int a,b,c;
+    c=(a=10,b=20,a+b);
+	cout<<a;
+	cout<<b;
+	cout<<c;    
+	return 0;
+}
