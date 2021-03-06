28ecc44468fcf1f8288ef3f3d59bb9829ce1dafc
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-06 08:11:05 -0800

    precendence of operator

---
+++ operatorexpression/15.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{
+    float result;
+    result=1.0+2.0*3.0/4.0;
+    cout<<result;
+    result=1.0/2.0+3.0;
+    cout<<result;
+    result=(1.0+2.0)/3.0;
+    cout<<result;
+    result=(1.0+2.0/3.0)+4.0;
+    cout<<result;
+	return 0;
+}
