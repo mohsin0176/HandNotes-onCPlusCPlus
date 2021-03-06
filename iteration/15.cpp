6e3a0dd90bcf61471fe415979d7c671395e0f8e8
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-06 08:54:52 -0800

    simple continue

---
+++ iteration/15.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{
+   int i;
+   for (int i = 0; i <=10; ++i)
+   {
+   	 if (i==5)
+   	 {
+   	 	continue;
+   	 }
+   	 cout<<i;
+   }
+	return 0;
+}
