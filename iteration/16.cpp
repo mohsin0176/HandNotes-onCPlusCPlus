34f494ebeb971037832e79651dd774d538b5cd04
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-06 08:55:53 -0800

    simple goto

---
+++ iteration/16.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{
+   int i=1;
+   BIIT:
+   cout<<i;
+   i++;
+   if (i<=10)
+   {
+      goto BIIT;
+   }
+	return 0;
+}
