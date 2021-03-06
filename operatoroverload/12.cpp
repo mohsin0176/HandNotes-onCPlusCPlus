5fbb63bd93a67a947f6f818ac14709443f7ae779
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-06 06:49:36 -0800

    Default-Parameterized constructor and default copy Constructor created by compiler

---
+++ operatoroverload/12.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+class A
+{
+private:
+	int x;
+public:
+	A(){}
+	A(int a){x=a;}
+	void showX(){cout<<x;}
+};
+
+int main()
+{
+    A a1(10);
+    A a2;
+    a1.showX();
+    a2=a1;
+    a2.showX();
+    A a3=a1;
+    a3.showX();
+    A a4(a1);
+    a4.showX();
+	return 0;
+}
