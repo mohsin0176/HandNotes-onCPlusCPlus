888a18d1b0d348978b8fd3d3789e81b95e9210a3
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-06 07:52:26 -0800

    Short-Hand Assignmnet - compound Assignment operator

---
+++ operatorexpression/5.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{
+    int num=25;
+    num+=10;
+    cout<<num;
+    num-=5;
+    cout<<num;
+    num*=2;
+    cout<<num;
+    num/=3;
+    cout<<num;
+    num%=3;
+    cout<<num;
+	return 0;
+}
