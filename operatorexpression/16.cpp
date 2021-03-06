f37126f94f1b453bc8d1775ba75c72e91d1d7a4c
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-06 08:12:27 -0800

    explicit conversion type casting

---
+++ operatorexpression/16.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{
+    int intv=20000;
+    intv=(long(intv)*10)/20;
+    cout<<intv;
+	return 0;
+}
