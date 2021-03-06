159d7db53f7705c2158f4ef2095440b1c743b31d
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-06 08:17:38 -0800

    explicit conversion type casting

---
+++ operatorexpression/19.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{
+    int a=23,b=456;
+    float c=46.65;
+    long d=24546,e;
+    e=(((long(a)+long(c))*d)+long(b));
+    cout<<e;
+	return 0;
+}
