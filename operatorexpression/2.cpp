a2d3b9fefc269befc1e0537d9d0893623c88d533
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-06 07:43:47 -0800

    relational operator

---
+++ operatorexpression/2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{  
+	int n;
+	cin>>n;
+	cout<<(n<10);
+	cout<<(n>10);
+	cout<<(n<=10);
+	cout<<(n>=10);
+	cout<<(n==10);	
+	cout<<(n!=10);
+	return 0;
+}
