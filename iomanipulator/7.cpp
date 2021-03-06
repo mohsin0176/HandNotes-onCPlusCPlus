ae71eeefa50ca21e1cd98abbb7be5e9b1a24f22c
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-06 09:43:48 -0800

    Cout.width function

---
+++ iomanipulator/7.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{
+    cout.width(8);
+    cout<<22<<endl;
+    cout.width(8);
+    cout<<4444<<endl;
+    cout.width(8);
+    cout<<666666<<endl;
+	return 0;
+}
