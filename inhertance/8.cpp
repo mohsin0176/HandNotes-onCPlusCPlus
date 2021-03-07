f493de1f5f69616453a528ca4ea9de6416a5a912
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-07 02:34:25 -0800

    private Visibility MOde

---
+++ inhertance/8.cpp
@@ -0,0 +1,49 @@
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+class A
+	{
+		int x;
+	public:
+		void getX()
+		{
+			cin>>x;
+		}
+		void showX()
+		{
+			cout<<x;
+		}
+	};
+class B:private A
+	{
+		int y;
+	public:
+		void getY()
+		{
+			cin>>y;
+		}
+		void showY()
+		{
+			cout<<y;
+		}
+
+	};
+
+
+int main()
+{   
+
+    B b1;
+    b1.getY();
+    b1.showY();
+
+
+
+	return 0;
+}
