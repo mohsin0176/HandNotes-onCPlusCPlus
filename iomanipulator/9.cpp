59d197258b480df59bc57f8c0e163b29e30ff6ff
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-07 01:24:17 -0800

    cout.precision()

---
+++ iomanipulator/9.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{
+    cout.precision(2)<<endl;
+    cout.width(8);
+    cout<<12.34567<<endl;
+    cout.width(8)<<endl;
+    cout<<12.1<<endl;
+	return 0;
+}
