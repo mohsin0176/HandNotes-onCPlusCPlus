92bf68d3e1e5e7c9946ab2a93ce80fe49577e31e
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-07 01:41:49 -0800

    use dec,oct,hex

---
+++ iomanipulator/20.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+//#define PI 3.1416
+using namespace std;
+
+
+int main()
+{   
+     int n;
+     cin>>n;
+     cout<<"Decimal Number"<<dec<<n;
+     cout<<"Octal Number"<<oct<<n;
+     cout<<"Hexa-Decimal Number"<<hex<<n;
+	return 0;
+}
