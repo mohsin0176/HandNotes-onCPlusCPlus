684724940449ea6c947f75eb9fbfcc55632587ae
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-07 02:54:55 -0800

    Inheritance with getter function in base class to access Private Data Member

---
+++ inhertance/12.cpp
@@ -0,0 +1,69 @@
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+class Circle
+{
+private:
+	float radius;
+public:
+	void readRadius()
+	{
+		cin>>radius;
+	}
+	float area()
+	{
+		float ar;
+		ar=PI*radius*radius;
+		return ar;
+	}
+	float getRadius()
+	{
+		return radius;
+	}
+	
+};
+
+class Cylinder:private Circle
+{
+private:
+float height;
+public:
+	void readHeight()
+	{
+		readRadius();
+		cin>>height;
+	}
+	float volume()
+	{
+		float v;
+		v=PI*getRadius()*getRadius()*height;
+		return v;
+	}
+	void showHeight()
+	{   
+		showRadius();
+		cout<<height;
+	}
+
+};
+
+int main()
+{   
+
+
+Cylinder cl1;
+cl1.readHeight();
+cl1.showHeight();
+float v=cl1.volume();
+cout<<v;
+
+
+
+	return 0;
+}
