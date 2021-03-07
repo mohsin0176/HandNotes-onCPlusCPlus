dd0107ff57b195cb019dbce8db48a94d0613796c
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-07 01:34:21 -0800

    cout.setf()

---
+++ iomanipulator/15.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+//#define PI 3.1416
+using namespace std;
+
+
+int main()
+{   
+    cout.setf(ios::showpos);
+    cout.setf(ios::showpoint);
+    cout.setf(ios::scientific,ios::floatfield);
+    cout.setprecision(2);
+    cout.width(8);
+    cout<<12.345678;
+    cout.width(8);
+    cout<<12.3<<endl;
+    cout.width(8);
+    cout<<12<<endl;
+	return 0;
+}
