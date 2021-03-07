8d077d2659cbd57405fe3675d80244874fd89ebf
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-07 01:26:42 -0800

    setprecision() manipulator

---
+++ iomanipulator/10.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{
+    cout<<setw(8)<<setprecision(2)<<endl;
+    cout<<12.34567<<endl;
+     cout<<setw(8)<<setprecision(2)<<endl;
+    cout<<12.1<<endl;
+	return 0;
+}
