de03a8a38465e4e8d24f92ed7c11229e696305e6
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-07 02:19:13 -0800

    multilevel inheritance

---
+++ inhertance/4.cpp
@@ -0,0 +1,68 @@
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+class A
+	{
+		int x;
+	public:
+		void getX()
+		{
+			cin>>x;
+		}
+		void showX()
+		{
+			cout<<x;
+		}
+	};
+class B:public A
+	{
+		int y;
+	public:
+		void getY()
+		{
+			cin>>y;
+		}
+		void showY()
+		{
+			cout<<y;
+		}
+
+	};
+
+class C:public B 
+	{
+		int z;
+	public:
+		void getZ()
+		{
+			cin>>z;
+		}
+		void showZ()
+		{
+			cout<<z;
+		}
+
+	};
+
+
+
+int main()
+{   
+
+    C c1;
+    c1.getX();
+    c1.getY();
+    c1.getZ();
+    b1.showX();
+    c1.showY();
+    c1.showX();
+    c1.showZ();
+
+	return 0;
+}
