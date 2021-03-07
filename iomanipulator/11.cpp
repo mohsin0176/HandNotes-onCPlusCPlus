6a607efce786b5992b97595b4477572ae1644fa3
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-07 01:28:11 -0800

    cout.fill()

---
+++ iomanipulator/11.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+#define PI 3.1416
+using namespace std;
+
+
+int main()
+{
+    cout.fill('*');
+    cout<<setw(8)<<22<<endl;
+    cout<<setw(8)<<4444<<endl;
+    cout<<setw(8)<<666666<<endl;
+	return 0;
+}
