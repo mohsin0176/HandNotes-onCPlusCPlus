d408d81438ae8c8ce61235c3d20f0361d537ab82
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-07 01:40:20 -0800

    use setbase

---
+++ iomanipulator/19.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+//#define PI 3.1416
+using namespace std;
+
+
+int main()
+{   
+     int n;
+     cin>>setbase(10)>>n;
+     cout<<"Decimal Number"<<setbase(10)<<n;
+     cout<<"Octal Number"<<setbase(8)<<n;
+     cout<<"Hexa-Decimal Number"<<setbase(16)<<n;
+	return 0;
+}
