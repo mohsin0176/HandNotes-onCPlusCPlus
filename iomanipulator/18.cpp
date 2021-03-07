eff933e9f2efac1fb0b3e51776ab4e66abb2136a
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-07 01:38:07 -0800

    use setiosflags

---
+++ iomanipulator/18.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+#include<cstdlib.h>
+#include<limits.h>
+#include<iomanip.h>
+#include<fstream.h>
+#include<process.h>
+//#define PI 3.1416
+using namespace std;
+
+
+int main()
+{   
+    cout.setiosflags(ios::showpos);
+    cout.setiosflags(ios::showpoint);
+    cout.setiosflags(ios::fixed|ios::left);
+    cout.setprecision(2);
+    cout<<setfill('*');
+    cout<<12.345678;
+    cout.width(8);
+    cout<<12.3<<endl;
+    cout.width(8);
+    cout<<12<<endl;
+	return 0;
+}
